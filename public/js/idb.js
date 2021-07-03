d6d5d5e6007aae40c842e283e16257a393e873e8
[Andrew Core] <[adcore89@gmail.com]>
2021-07-03 09:22:10 -0400

    idb started

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,1 +1,33 @@
 //create database connection 
+//create variable to hold db connection 
+let db;
+//establish connection to indexedDB called budget-trackerdb
+const request =indexedDB.open('budget_trackerdb', 1);
+
+// add object store 
+request.onupgradeneeded = function(event){
+    const db = event.target.result;
+    db.createObjectStore('new_budget', {autoIncrement: true});
+};
+
+//upon success 
+request.onsuccess = function(event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+}
+
+// function to be executed if we attempt to submit new transaction with no internet 
+function saveRecord(record) {
+    const transaction = db.transaction ([ 'new_budget'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_budget');
+
+    budgetObjectStore.add(record);
+}
